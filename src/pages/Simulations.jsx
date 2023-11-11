c8cc735f0cc515dc5de173b84ae728824174e86a
Juan Ortega <A01794327@tec.mx>
2023-11-11 17:14:01 -0600

    mejora de edicion de simulaciones guardadas

--- src/pages/Simulations.jsx
+++ src/pages/Simulations.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Box, Button, FormControl, FormLabel, Input, Select, Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
 import axios from 'axios';
+import { calculatePayments, calculateEarnings } from '../formulas';
 
 const Simulations = () => {
     const [products, setProducts] = useState([]);
@@ -23,51 +24,14 @@ const Simulations = () => {
         fetchProducts();
     }, []);
 
-    const calculatePayments = () => {
-        // Filter products where type is 'credit'
-        const filteredProducts = products.filter(product => product.type === 'credito');
-
-        return filteredProducts.map(product => {
-            const monthlyRate = product.apr / 1200; // Convert APR to a monthly rate
-            const totalPayments = period; // Total number of monthly payments
-            const presentValue = amount; // Loan amount
-
-            // Calculate monthly payment using the compound interest formula
-            const monthlyPayment = monthlyRate * presentValue / (1 - Math.pow(1 + monthlyRate, -totalPayments));
-
-            return { ...product, monthlyPayment: isNaN(monthlyPayment) ? 0 : monthlyPayment };
-        });
-    };
-
-    const calculateEarnings = () => {
-        // Filter products where type is 'invest'
-        const filteredProducts = products.filter(product => product.type === 'inversion');
-
-        return filteredProducts.map(product => {
-            const monthlyRate = product.apr / 1200; // Convert APR to a monthly rate
-            const totalMonths = period; // Total number of months
-
-            // Calculate the future value using the compound interest formula
-            const futureValue = amount * Math.pow(1 + monthlyRate, totalMonths);
-
-            // Calculate interest earned by subtracting principal from future value
-            const interestEarned = futureValue - amount;
-
-            return { ...product, monthlyPayment: isNaN(interestEarned) ? 0 : interestEarned };
-        });
-    };
-
-
-
-
     const handleSubmit = (e) => {
         e.preventDefault();
         if (type === 'credito') {
-            const calculatedSimulations = calculatePayments();
+            const calculatedSimulations = calculatePayments(products, amount, period);
             setSimulations(calculatedSimulations);
         } else {
             console.log("Entro a calcular inversion");
-            const calculateEarningspayments = calculateEarnings();
+            const calculateEarningspayments = calculateEarnings(products, amount, period);
             setSimulations(calculateEarningspayments);
         }
     };
