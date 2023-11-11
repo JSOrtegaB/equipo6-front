c01bb1fdd6e670724ae08961d0520692a467b4fc
Sebastian Ortega M2 <sortega@moviltrack.com>
2023-11-11 13:35:54 -0600

    trabajando en edicion de tabla simulaciones

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Box, Flex } from '@chakra-ui/react';
+import FinancialEducationPortal from '../components/FinancialEducationPortal';
 
 const Home = () => {
   return (
     <Flex>
       <Box ml="200px"> {/* Content should start after the sidebar */}
-        Pagina de Inicio
+        <FinancialEducationPortal />
       </Box>
     </Flex>
   );
